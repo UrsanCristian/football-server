3f38a47ff655f1d441724760211ee6ec65b14e2c
beckfe <d7726308h@Student.DHBW-VS.DE>
2025-02-06 10:16:26 +0100

    Implement GameController

--- src/main/java/de/guehring/footballserver/game/GameController.java
+++ src/main/java/de/guehring/footballserver/game/GameController.java
@@ -1,4 +1,34 @@
 package de.guehring.footballserver.game;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/games")
 public class GameController {
+    private final GameService gameService;
+
+    public.
+
+    GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping
+    public List<GameDTO> getGames() {
+        return gameService.getAllGames();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO getGameById(@PathVariable int id) {
+        return playerService.getGameById(id);
+    }
+
+    @GetMapping
+    public String createNewGame(@RequestParam GameDTO gameDTO) {
+        gameService.createNewGame(GameDTO);
+        return "New Game created";
+    }
 }
